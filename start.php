b5c4ada05ee08c75162fb88e921c6ca1867d4898
TimeFinger <hewn2011@126.com>
2018-12-22 21:46:23 +0800

    以composer的形式包装

--- start.php
+++ start.php
@@ -1,11 +1,11 @@
 <?php
 
+require './vendor/autoload.php';
+
 if (isset($argv[1])) {
     if ($argv[1] == 'server') {
-        require 'server.php';
         new \TimeFinger\Socks5Server();
     } elseif ($argv[1] == 'client') {
-        require 'client.php';
         new \TimeFinger\Socks5Client();
     }
 }

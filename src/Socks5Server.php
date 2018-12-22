99818d7c294acef357c064f66afb2606cb0835f2
TimeFinger <hewn2011@126.com>
2018-12-22 22:01:39 +0800

    精简代码:抽出公共常为interface

--- src/Socks5Server.php
+++ src/Socks5Server.php
@@ -2,51 +2,8 @@
 
 namespace TimeFinger;
 
-class Socks5Server
+class Socks5Server implements ConstantInterface
 {
-    const VER = 0x05;
-
-    /**
-     * 各请求阶段定义
-     */
-    const STAGE_INIT = 0;
-    const STAGE_ADDRESSING = 1;
-    const STAGE_REQUEST = 2;
-
-    /**
-     * 初始化阶段METHODS定义
-     */
-    const METHOD_NOAUTH = 0x00;
-    const METHOD_GSSAPI = 0x01;
-    const METHOD_USERPASS = 0x02;
-    const METHOD_INNA = [0x03, 0x7F];
-    const METHOD_PRIVATE = [0x80, 0xFE];
-    const METHOD_NOACCEPT = 0xFF;
-
-    /**
-     * ADDRESSING客户端请求阶段CMD定义
-     */
-    const CMD_CONNECT = 0x01;
-    const CMD_BIND = 0x02;
-    const CMD_UDP_ASSOCIATE = 0x03;
-
-    /**
-     * 一些公用字段定义
-     */
-    const COMM_RSV = 0x00;
-    const COMM_ATYPE_IPV4 = 0x01;
-    const COMM_ATYPE_DOMAIN = 0x03;
-    const COMM_ATYPE_IPV6 = 0x04;
-
-    /**
-     * ADDRESSING服务端响应阶段REP定义
-     */
-    const REP_SUCC = 0x00;
-    // todo other status
-
-
-    const CONNECT_TIMEOUT = 10;  // 设置超时时间为10s
-
     public $method;
 
     public $clients = [];
